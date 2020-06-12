3b310e2198865f64d1adfe0f171d62c1ddf2472f
overlookmotel <theoverlookmotel@gmail.com>
2020-06-12 14:30:56 +0100

    Example

--- example/src/index.js
+++ example/src/index.js
@@ -4,21 +4,15 @@ let a = 1;
 function outer() {
 	let b = a;
 
-	if (b) {
-		const c = b;
-		return function inner() {
-			console.log(`a = ${a}, b = ${b}, c = ${c}`); // eslint-disable-line no-console
-			a++;
-			b += 100;
-		};
-	}
-	return () => {};
+	return function inner() {
+		console.log(`a = ${a}, b = ${b}`); // eslint-disable-line no-console
+		a++;
+		b += 100;
+	};
 }
 
 const inner1 = outer();
 const inner2 = outer();
-inner1();
-inner2();
 inner2();
 
-module.exports = {inner1, inner2, outer};
+module.exports = {inner1, inner2};
