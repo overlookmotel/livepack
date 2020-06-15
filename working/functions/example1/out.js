284f5e1abe335d957f315dcaa93df7b4593f9c47
overlookmotel <theoverlookmotel@gmail.com>
2020-06-15 18:25:11 +0100

    Temp file

--- working/functions/example1/out.js
+++ working/functions/example1/out.js
@@ -2,22 +2,18 @@
 
 'use strict';
 
-function createScope0() {
-	return [function createScope1(a) {
-		return [function createScope2(b) {
-			function inner() {
-				console.log(`a = ${a}, b = ${b}`);
-				a++;
-				b += 100;
-			}
-			return [inner];
-		}];
+function createBlock1(a) {
+	return [function createBlock2(b) {
+		function inner() {
+			console.log(`a = ${a}, b = ${b}`);
+			a++;
+			b += 100;
+		}
+		return [inner];
 	}];
 }
 
-const scope0 = createScope0(),
-	createScope1 = scope0[0],
-	scope1 = createScope1(2),
+const scope1 = createBlock1(2),
 	createScope2 = scope1[0],
 	scope2 = createScope2(1),
 	scope3 = createScope2(101),
@@ -27,5 +23,5 @@ const scope0 = createScope0(),
 module.exports = c;
 
 // Compacts down to:
-// const createScope2 = createScope0()[0](2)[0];
+// const createScope2 = createScope1(2)[0];
 // module.exports = {inner1: createScope2(1)[0], inner2: createScope2(101)[0]};
