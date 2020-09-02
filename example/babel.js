e96a50b14d575c09418ff47cba1468e8652cca5d
overlookmotel <theoverlookmotel@gmail.com>
2020-09-03 00:50:49 +0100

    Example

--- example/babel.js
+++ example/babel.js
@@ -6,6 +6,9 @@
 const pathJoin = require('path').join,
 	{readFileSync} = require('fs'),
 	{transformSync} = require('@babel/core'),
+	pluginModulesToCommonJs = require('@babel/plugin-transform-modules-commonjs').default,
+	pluginImportMeta = require('babel-plugin-transform-import-meta').default,
+	pluginTransformJsx = require('@babel/plugin-transform-react-jsx').default,
 	babelPlugin = require('../babel.js'); // require('livepack/babel')
 
 // Run
@@ -14,13 +17,22 @@ console.log('--------------------');
 console.log('--------------------');
 console.log('--------------------');
 
+const esm = true,
+	jsx = false;
+
 const path = pathJoin(__dirname, 'src/index.js');
 const inputJs = readFileSync(path, 'utf8');
 const outputJs = transformSync(inputJs, {
-	plugins: [babelPlugin],
+	plugins: [
+		// Transform JSX if `jsx` option set
+		...(jsx ? [pluginTransformJsx] : []),
+		// Convert ESM to CJS if `esm` option set
+		...(esm ? [pluginModulesToCommonJs, pluginImportMeta] : []),
+		babelPlugin
+	],
 	filename: path,
 	generatorOpts: {retainLines: true},
-	sourceType: 'script'
+	sourceType: esm ? 'module' : 'script'
 }).code;
 
 console.log(outputJs);
