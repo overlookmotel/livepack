7b2df98be1ceac181825decdbc2f7cbcc117b3a2
overlookmotel <theoverlookmotel@gmail.com>
2020-09-09 17:36:20 +0100

    Babel plugin: Create all internal vars with common prefix [refactor]

--- test/misc.test.js
+++ test/misc.test.js
@@ -14,17 +14,28 @@ const ext = {};
 
 describeWithAllOptions('Internal vars created by Babel plugin do not interfere with code', ({run}) => {
 	it('`tracker`', () => {
+		// Check the temp var name which Babel transform creates matches the one being tested for
+		// NB code for this file is injected into `files` in babel transform
+		// (see `test/support/transform.js`)
+		expect(transpiledFiles[__filename].code).toMatch(/const( )livepack1_tracker = require\("/);
+
 		run(
-			() => typeof tracker,
-			'()=>typeof tracker',
+			() => typeof livepack_tracker, // eslint-disable-line camelcase
+			'()=>typeof livepack_tracker',
 			fn => expect(fn()).toBe('undefined')
 		);
 	});
 
 	it('`scopeId`', () => {
+		// Check the temp var name which Babel transform creates matches the one being tested for
+		// NB code for this file is injected into `files` in babel transform
+		// (see `test/support/transform.js`)
+		expect(transpiledFiles[__filename].code)
+			.toMatch(/const( )livepack1_scopeId_1 = livepack1_tracker\(\);/);
+
 		run(
-			() => ext && typeof scopeId_1, // eslint-disable-line camelcase
-			'(a=>()=>a&&typeof scopeId_1)({})',
+			() => ext && typeof livepack_scopeId_1, // eslint-disable-line camelcase
+			'(a=>()=>a&&typeof livepack_scopeId_1)({})',
 			fn => expect(fn()).toBe('undefined')
 		);
 	});
@@ -33,14 +44,14 @@ describeWithAllOptions('Internal vars created by Babel plugin do not interfere w
 		// Check the temp var name which Babel transform creates matches the one being tested for
 		// NB code for this file is injected into `files` in babel transform
 		// (see `test/support/transform.js`)
-		expect(transpiledFiles[__filename].code).toMatch(/\/\*livepack_temp:assign\*\/temp1_11/);
+		expect(transpiledFiles[__filename].code).toMatch(/\/\*livepack_temp:assign\*\/livepack1_temp_11/);
 
 		run(
 			Object.setPrototypeOf(
-				{x() { return super.x(typeof temp_11); }}, // eslint-disable-line camelcase
+				{x() { return super.x(typeof livepack_temp_11); }}, // eslint-disable-line camelcase
 				{x(v) { return v; }}
 			),
-			'(()=>{const a=(a=>[b=>a=b,{x(){return Reflect.get(Object.getPrototypeOf(a),"x",this).call(this,typeof temp_11)}}.x])(),b=Object,c=b.assign(b.create({x(a){return a}}),{x:a[1]});a[0](c);return c})()',
+			'(()=>{const a=(a=>[b=>a=b,{x(){return Reflect.get(Object.getPrototypeOf(a),"x",this).call(this,typeof livepack_temp_11)}}.x])(),b=Object,c=b.assign(b.create({x(a){return a}}),{x:a[1]});a[0](c);return c})()',
 			obj => expect(obj.x()).toBe('undefined')
 		);
 	});
