e2020c7fabc6f8cf526a9a9ba135a26fbd0f3b40
overlookmotel <theoverlookmotel@gmail.com>
2022-11-05 14:56:04 +0000

    Tests: `LIVEPACK_DEBUG_REGISTER` env [improve]

--- test/support/transform.js
+++ test/support/transform.js
@@ -18,19 +18,30 @@ const {instrumentCodeImpl} = require('../../lib/instrument/instrument.js'),
 
 // Constants
 const TESTS_SUPPORT_DIR_PATH = `${__dirname}${pathSep}`,
-	TRANSPILED_FILES_PATH = `${TESTS_SUPPORT_DIR_PATH}transpiledFiles.js`;
+	TRANSPILED_FILES_PATH = `${TESTS_SUPPORT_DIR_PATH}transpiledFiles.js`,
+	DEBUG = !!process.env.LIVEPACK_DEBUG_REGISTER;
 
 // Exports
 
-module.exports = {process};
+module.exports = {process: processFile};
 
-function process(code, filename) {
+function processFile(code, filename) {
 	// Do not instrument Livepack internals or tests support files
 	if (isInternalPath(filename) || filename.startsWith(TESTS_SUPPORT_DIR_PATH)) return {code};
 
 	// Instrument code
-	const res = instrumentCodeImpl(code, filename, false, true, false, false, true, undefined, true);
+	const res = instrumentCodeImpl(code, filename, false, true, false, false, true, undefined, !DEBUG);
 	code = res.code;
+
+	if (DEBUG) {
+		/* eslint-disable no-console */
+		console.log('----------------------------------------');
+		console.log('TRANSFORMED:', filename);
+		console.log('----------------------------------------');
+		console.log(code);
+		console.log('');
+		/* eslint-enable no-console */
+	}
 
 	// Add `require('livepack/test/support/transpiledFiles.js')[__filename] = '<code>';` to end of code
 	code += `\nrequire(${JSON.stringify(TRANSPILED_FILES_PATH)})[__filename] = ${JSON.stringify(code)};`;
