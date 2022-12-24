857df3a63d51859e20272b7104158e54f84d0b05
overlookmotel <theoverlookmotel@gmail.com>
2022-12-24 21:57:55 +0000

    Refactor determining internal/runtime file paths [refactor]

--- test/support/transform.js
+++ test/support/transform.js
@@ -10,15 +10,15 @@
 'use strict';
 
 // Modules
-const pathSep = require('path').sep;
+const {join: pathJoin, sep: pathSep} = require('path');
 
 // Imports
-const {instrumentCodeImpl} = require('../../lib/instrument/instrument.js'),
-	{isInternalPath} = require('../../lib/shared/functions.js');
+const {instrumentCodeImpl} = require('../../lib/instrument/instrument.js');
 
 // Constants
-const TESTS_SUPPORT_DIR_PATH = `${__dirname}${pathSep}`,
-	TRANSPILED_FILES_PATH = `${TESTS_SUPPORT_DIR_PATH}transpiledFiles.js`,
+const LIVEPACK_DIR_PATH = pathJoin(__dirname, '../../'),
+	LIVEPACK_DIR_PATH_LEN = LIVEPACK_DIR_PATH.length,
+	TRANSPILED_FILES_PATH = pathJoin(__dirname, 'transpiledFiles.js'),
 	DEBUG = !!process.env.LIVEPACK_DEBUG_INSTRUMENT;
 
 // Exports
@@ -27,7 +27,7 @@ module.exports = {process: processFile};
 
 function processFile(code, filename) {
 	// Do not instrument Livepack internals or tests support files
-	if (isInternalPath(filename) || filename.startsWith(TESTS_SUPPORT_DIR_PATH)) return {code};
+	if (shouldNotInstrument(filename)) return {code};
 
 	// Instrument code
 	const res = instrumentCodeImpl(code, filename, false, true, false, false, true, undefined, !DEBUG);
@@ -47,4 +47,29 @@ function processFile(code, filename) {
 	code += `\nrequire(${JSON.stringify(TRANSPILED_FILES_PATH)})[__filename] = ${JSON.stringify(code)};`;
 
 	return {code, map: res.map};
+}
+
+/**
+ * Determine if a file path should be instrumented.
+ *
+ * Files bypassing instrumentation are:
+ * - Top level files e.g. `index.js`
+ * - `lib/*` except for `lib/runtime/*`
+ * - `test/support/*`
+ *
+ * Files in `lib/runtime` are excluded as they contains code which is serialized and used in output.
+ *
+ * @param {string} path - File path
+ * @returns {boolean} - `true` if is a file in Livepack's codebase which should not be transformed
+ */
+function shouldNotInstrument(path) {
+	if (!path.startsWith(LIVEPACK_DIR_PATH)) return false;
+
+	const pathParts = path.slice(LIVEPACK_DIR_PATH_LEN).split(pathSep, 2);
+	if (pathParts.length === 1) return true;
+	switch (pathParts[0]) {
+		case 'lib': return pathParts[1] !== 'runtime';
+		case 'test': return pathParts[1] === 'support';
+		default: return false;
+	}
 }
