fc4fa3b99e769a23fbf2df36cb68e60e882bebe8
overlookmotel <theoverlookmotel@gmail.com>
2023-01-02 17:59:07 +0000

    Dev: Replace `@overlook/jest-extended` with `jest-extended`
    Closes #466.

--- jest.config.js
+++ jest.config.js
@@ -14,7 +14,7 @@ module.exports = {
 	coverageProvider: 'v8',
 	collectCoverageFrom: ['*.js', '!.eslintrc.js', '!jest.config.js', 'lib/**/*.js'],
 	setupFilesAfterEnv: [
-		'@overlookmotel/jest-extended/all',
+		'jest-extended/all',
 		'jest-expect-arguments',
 		'<rootDir>/test/support/expect.js',
 		'<rootDir>/test/support/register.js'
