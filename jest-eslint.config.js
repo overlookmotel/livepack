5754f236905df803151d5dac7cadc07a655f6ab1
overlookmotel <theoverlookmotel@gmail.com>
2023-09-02 20:19:53 +0100

    Dev: ESLint run on largest files first [fix]

--- jest-eslint.config.js
+++ jest-eslint.config.js
@@ -10,6 +10,7 @@
 module.exports = {
 	testEnvironment: 'node',
 	runner: 'jest-runner-eslint',
+	testSequencer: '<rootDir>/test/support/eslintSequencer.js',
 	testMatch: ['<rootDir>/**/*.(js|cjs|mjs|jsx)'],
 	// Jest by default uses a number of workers equal to number of CPU cores minus 1.
 	// Github Actions runners provide 2 cores and running with 2 workers is faster than 1.
