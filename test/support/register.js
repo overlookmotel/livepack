b83e7ee498a34152a9844e419db8e996fe0a07a0
overlookmotel <theoverlookmotel@gmail.com>
2023-09-03 01:47:29 +0100

    Tests: Avoid instrumenting modules used by testing infrastructure [fix]
    Fixes #474.

--- test/support/register.js
+++ test/support/register.js
@@ -3,12 +3,32 @@
  * Tests install require hook to intrument test files' code
  * ------------------*/
 
-/* eslint-disable import/order, import/newline-after-import */
+/* eslint-disable import/order, import/no-dynamic-require, global-require */
 
 'use strict';
 
+// Imports
+const {
+	useInternalModuleCache, useGlobalModuleCache, usingInternalModuleCache, Module
+} = require('../../lib/shared/moduleCache.js');
+
 // Patch filesystem for virtual fixtures files
 require('./fixturesFs.js');
+
+// Pre-load `fast-json-stable-stringify`.
+// `jest-light-runner` imports `jest-snapshot` which requires `@jest/transform`.
+// After `jest-light-runner`'s worker loads the test file, it runs a method on `jest-snapshot`
+// which causes `@jest/transform` to lazily require `fast-json-stable-stringify`.
+// Trigger this now, so `fast-json-stable-stringify` is not loaded later and unnecessarily instrumented.
+// `createScriptTransformer()` when called with no arguments throws an error before it does anything.
+const {createRequire} = Module,
+	jestLightRunnerPath = require.resolve('jest-light-runner'),
+	jestSnapshotPath = createRequire(jestLightRunnerPath).resolve('jest-snapshot'),
+	jestTransformPath = createRequire(jestSnapshotPath).resolve('@jest/transform');
+if (Module._cache[jestTransformPath]) {
+	const jestTransform = require(jestTransformPath);
+	jestTransform.createScriptTransformer().catch(() => {});
+}
 
 // Install register require hook.
 // Disable instrumentation cache. Tests run in multiple threads, so it'd be read from and written to
@@ -16,9 +36,6 @@ require('./fixturesFs.js');
 require('../../register.js')({cache: false});
 
 // Use internal module cache to avoid transpiling modules
-const {
-	useInternalModuleCache, useGlobalModuleCache, usingInternalModuleCache
-} = require('../../lib/shared/moduleCache.js');
 useInternalModuleCache();
 
 // Modules
