cef83891203b5981b3bc7cd7dc7d5bbf12f101b3
overlookmotel <theoverlookmotel@gmail.com>
2023-10-04 00:24:14 +0100

    Shorter output for buffers containing zeros [improve]

--- test/buffers.test.js
+++ test/buffers.test.js
@@ -67,56 +67,56 @@ describe('Buffers', () => {
 	describe('typedArrays', () => {
 		describe('standard', () => {
 			itSerializesEqual('Uint8Array', {
-				in: () => new Uint8Array([100, 200]),
-				out: 'new Uint8Array([100,200])',
+				in: () => new Uint8Array([100, 0, 200]),
+				out: 'new Uint8Array([100,,200])',
 				validate(buf) {
 					expect(buf).toBeInstanceOf(Uint8Array);
-					expect([...buf]).toEqual([100, 200]);
+					expect([...buf]).toEqual([100, 0, 200]);
 				}
 			});
 
 			itSerializesEqual('Int8Array', {
-				in: () => new Int8Array([100, -56]),
-				out: 'new Int8Array([100,-56])',
+				in: () => new Int8Array([100, 0, -56]),
+				out: 'new Int8Array([100,,-56])',
 				validate(buf) {
 					expect(buf).toBeInstanceOf(Int8Array);
-					expect([...buf]).toEqual([100, -56]);
+					expect([...buf]).toEqual([100, 0, -56]);
 				}
 			});
 
 			itSerializesEqual('Uint16Array', {
-				in: () => new Uint16Array([1000, 40000]),
-				out: 'new Uint16Array([1000,40000])',
+				in: () => new Uint16Array([1000, 0, 40000]),
+				out: 'new Uint16Array([1000,,40000])',
 				validate(buf) {
 					expect(buf).toBeInstanceOf(Uint16Array);
-					expect([...buf]).toEqual([1000, 40000]);
+					expect([...buf]).toEqual([1000, 0, 40000]);
 				}
 			});
 
 			itSerializesEqual('Int16Array', {
-				in: () => new Int16Array([1000, -1000]),
-				out: 'new Int16Array([1000,-1000])',
+				in: () => new Int16Array([1000, 0, -1000]),
+				out: 'new Int16Array([1000,,-1000])',
 				validate(buf) {
 					expect(buf).toBeInstanceOf(Int16Array);
-					expect([...buf]).toEqual([1000, -1000]);
+					expect([...buf]).toEqual([1000, 0, -1000]);
 				}
 			});
 
 			itSerializesEqual('Uint32Array', {
-				in: () => new Uint32Array([100000, 40000000]),
-				out: 'new Uint32Array([100000,40000000])',
+				in: () => new Uint32Array([100000, 0, 40000000]),
+				out: 'new Uint32Array([100000,,40000000])',
 				validate(buf) {
 					expect(buf).toBeInstanceOf(Uint32Array);
-					expect([...buf]).toEqual([100000, 40000000]);
+					expect([...buf]).toEqual([100000, 0, 40000000]);
 				}
 			});
 
 			itSerializesEqual('Int32Array', {
-				in: () => new Int32Array([100000, -100000]),
-				out: 'new Int32Array([100000,-100000])',
+				in: () => new Int32Array([100000, 0, -100000]),
+				out: 'new Int32Array([100000,,-100000])',
 				validate(buf) {
 					expect(buf).toBeInstanceOf(Int32Array);
-					expect([...buf]).toEqual([100000, -100000]);
+					expect([...buf]).toEqual([100000, 0, -100000]);
 				}
 			});
 		});
@@ -303,14 +303,14 @@ describe('Buffers', () => {
 		itSerializes('not zeroed', {
 			in() {
 				const buf = new ArrayBuffer(8);
-				new Uint8Array(buf).set([0, 1, 2, 3, 4, 15, 16, 0]);
+				new Uint8Array(buf).set([0, 1, 2, 3, 0, 15, 16, 0]);
 				return buf;
 			},
-			out: 'new Uint8Array([0,1,2,3,4,15,16,0]).buffer',
+			out: 'new Uint8Array([,1,2,3,,15,16,,]).buffer',
 			validate(buf) {
 				expect(buf).toHavePrototype(ArrayBuffer.prototype);
 				expect(buf.byteLength).toBe(8);
-				expect([...new Uint8Array(buf)]).toEqual([0, 1, 2, 3, 4, 15, 16, 0]);
+				expect([...new Uint8Array(buf)]).toEqual([0, 1, 2, 3, 0, 15, 16, 0]);
 			}
 		});
 
@@ -358,18 +358,38 @@ describe('Buffers', () => {
 			}
 		});
 
-		itSerializes('not zeroed', {
-			in() {
-				const buf = new SharedArrayBuffer(8);
-				new Uint8Array(buf).set([2, 3, 0, 15], 2);
-				return buf;
-			},
-			out: '(()=>{const a=new SharedArrayBuffer(8);new Uint8Array(a).set([2,3,0,15],2);return a})()',
-			validate(buf) {
-				expect(buf).toHavePrototype(SharedArrayBuffer.prototype);
-				expect(buf.byteLength).toBe(8);
-				expect([...new Uint8Array(buf)]).toEqual([0, 0, 2, 3, 0, 15, 0, 0]);
-			}
+		describe('not zeroed', () => {
+			itSerializes('with all bytes non-zero', {
+				in() {
+					const buf = new SharedArrayBuffer(8);
+					new Uint8Array(buf).set([1, 2, 3, 4, 5, 6, 7, 16]);
+					return buf;
+				},
+				out: `(()=>{
+					const a=new SharedArrayBuffer(8);
+					new Uint8Array(a).set([1,2,3,4,5,6,7,16]);
+					return a
+				})()`,
+				validate(buf) {
+					expect(buf).toHavePrototype(SharedArrayBuffer.prototype);
+					expect(buf.byteLength).toBe(8);
+					expect([...new Uint8Array(buf)]).toEqual([1, 2, 3, 4, 5, 6, 7, 16]);
+				}
+			});
+
+			itSerializes('with zero bytes start and end', {
+				in() {
+					const buf = new SharedArrayBuffer(8);
+					new Uint8Array(buf).set([0, 0, 2, 3, 0, 15, 0, 0]);
+					return buf;
+				},
+				out: '(()=>{const a=new SharedArrayBuffer(8);new Uint8Array(a).set([2,3,,15],2);return a})()',
+				validate(buf) {
+					expect(buf).toHavePrototype(SharedArrayBuffer.prototype);
+					expect(buf.byteLength).toBe(8);
+					expect([...new Uint8Array(buf)]).toEqual([0, 0, 2, 3, 0, 15, 0, 0]);
+				}
+			});
 		});
 
 		describe('with prototype set to', () => {
